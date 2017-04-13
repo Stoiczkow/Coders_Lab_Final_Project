16d1e63bff9fd4d2786c341a0a1bf1a1d599cfbc
Pawel Wyzykowski <pawelwyzykowski@gmail.com>
2017-04-13 11:47:53 +0200

    added selection of tasks by date range (employee view)

--- employee_management/employees/static/js/date_range.js
+++ employee_management/employees/static/js/date_range.js
@@ -6,17 +6,21 @@ btn.addEventListener("click", function (event) {
 
 var from_date = document.querySelector("#from")
 var to_date = document.querySelector("#to")
-var tasks = document.querySelectorAll("p")
+var tasks = document.querySelectorAll(".label-info")
+from_date = new Date(from_date.value)
+to_date = new Date(to_date.value)
 
-console.log(from_date.value)
-console.log(to_date.value)
-console.log(tasks)
 for(var i = 0; i < tasks.length; i++){
-    console.log(tasks[i].value2)
-}
-if(from_date.value <= to_date.value){
+    var compare_date = new Date(tasks[i].getAttribute("value2"))
+
+    if(from_date <= compare_date  && compare_date <= to_date){
+        tasks[i].classList.remove("hidden")
+    }else{
+        tasks[i].classList.add("hidden")
+    }
 
 }
+
 });
 
 });
